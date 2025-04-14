8008b98ff9662d77ad95fe690870032926edb6fc
Nandan Bhowmick <nandanbhow123@gmail.com>
2025-04-15 03:46:22 +0600

    completed maven task

---
+++ SE606-Software Design and Analysis/Maven/Task 1/my-stringlib/src/main/java/com/example/MyStringLib.java
@@ -0,0 +1,8 @@
+package com.example;
+import org.apache.commons.lang3.StringUtils;
+
+public class MyStringLib {
+    public static String formatString(String str){
+        return StringUtils.upperCase(str);
+    }
+}
---
+++ SE606-Software Design and Analysis/Maven/Task 1/my-utils/src/main/java/com/example/MyUtils.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyUtils {
+    public static void greet(String name){
+        System.out.println("Hello "+name+"!");
+    }
+}
